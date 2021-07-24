653692b362558854c4849df6418f19d4a0617b31
devchoplife <dritedurojaiye@gmail.com>
2021-07-24 17:27:06 +0100

    Fix bugs and typos

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
-package main 
+package main
 
-import {
+import (
 	"encoding/json"
 	"log"
 	"math/rand"
@@ -8,62 +8,63 @@ import {
 	"strconv"
 
 	"github.com/gorilla/mux"
-}
+)
 
-//Book struct 
+//Book struct
 
 type Book struct {
-	ID string `json:"id"`
-	Isbn string `json:"isbn"`
-	Title string `json:"title"`
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
 	Author *Author `json:"author"`
 }
 
 type Author struct {
 	Firstname string `json:"firstname"`
-	Lastname string`json:"lastname"`
+	Lastname  string `json:"lastname"`
 }
 
-//slice to hold the books 
-books := []Book
+//slice to hold the books
+var books []Book
 
-//Get all books 
+//Get all books
 func getBooks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(books)
 }
 
-//Get a single book 
+//Get a single book
 func getBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r) //Get params 
-	//Loop through the books and find the one with the ID from the params 
-	for _, item :- range books {
+	params := mux.Vars(r) //Get params
+	//Loop through the books and find the one with the ID from the params
+	for _, item := range books {
 		if item.ID == params["id"] {
 			json.NewEncoder(w).Encode(item)
 		}
 	}
-	json.NewEncoder(w).Encode(&book{})
+	json.NewEncoder(w).Encode(&Book{})
 }
 
-//Add new book 
+//Add new book
 func addNewBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var book Book
-	_ = json.NewDecoder(r.Body).Decode($book)
-	book.ID = strvonv.Itoa(rand.Intn(100000000)) //Mock ID - Not safe 
-	books = append(books, book )
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(100000000)) //Mock ID - Not safe
+	books = append(books, book)
 	json.NewEncoder(w).Encode(book)
 }
 
-//Update book 
+//Update book
 func updateBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	params :- mux.Vars(r)
+	params := mux.Vars(r)
 	for index, item := range books {
 		if item.ID == params["id"] {
-			books = append(books[:index], books[index + 1:]...)
-			var book Book = json.NewDecoder(r.Body).Decode(&book)
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
 			book.ID = params["id"]
 			books = append(books, book)
 			json.NewEncoder(w).Encode(book)
@@ -72,33 +73,33 @@ func updateBook(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-//Delete book 
-func deleteBook (w http.ResponseWriter, r *http.Request) {
+//Delete book
+func deleteBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	for index, item := range books {
 		if item.ID == params["id"] {
-			books - append(books[:index], books[index+1:])
+			books = append(books[:index], books[index+1:]...)
 			break
 		}
 	}
 }
 
-//Main 
+//Main
 func main() {
-	r = mux.NewRouter()
+	r := mux.NewRouter()
 
-	//Add data to the books database 
+	//Add data to the books database
 	books = append(books, Book{ID: "1", Isbn: "438227", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Doe"}})
 	books = append(books, Book{ID: "2", Isbn: "454555", Title: "Book Two", Author: &Author{Firstname: "Steve", Lastname: "Smith"}})
 
-	//Route handles and endpoints 
-	r.HandleFunc("/books", getBooks).methods("GET")
-	r.HandleFunc("/books/{id}", getBooks).methods("GET")
-	r.HandleFunc("/books", addNewBook).methods("POST")
-	r.HandleFunc("/books/{id}", updateBook).methods("PUT")
-	r.HandleFunc("/books", getBooks).methods("GET")
+	//Route handles and endpoints
+	r.HandleFunc("/books", getBooks).Methods("GET")
+	r.HandleFunc("/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/books", addNewBook).Methods("POST")
+	r.HandleFunc("/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/books", deleteBook).Methods("GET")
 
-	//Start server 
+	//Start server
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
